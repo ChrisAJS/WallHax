6036f75fed37a814afccd961c155d18d067bb4a2
Matthew Mould <mouldm02@jimmyluong-wifi.dev.pod.bbc>
2013-12-18 14:02:05 +0000

    Initial commit of csv json parser

---
+++ csvjsonparser/wallhax.js
@@ -0,0 +1,32 @@
+var jsonObject = 
+	[{
+	    "columnName": "todo",
+	    "tickets": []
+	}, {
+	    "columnName": "indev",
+	    "tickets": []
+	}, {
+	    "columnName": "done",
+	    "tickets": []
+	}];
+
+var columnWidth = 682;
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (lines) {
+	lines.split(/\n/).forEach(function(line){
+		if ((/.+,.+,.+/).test(line)) {
+			splitLine = line.split(/,/);
+			var ticketId = splitLine[0];
+	 		var xPos = splitLine[1];
+	 		var columnInd = Math.floor(xPos/columnWidth);
+	 		columnInd = Math.min(columnInd, 2);
+	 		columnInd = Math.max(columnInd, 0);
+	 		jsonObject[columnInd].tickets.push(ticketId);
+		}
+	})
+	console.log(jsonObject);
+});
+
